78833687933c0a7dd81f301995795b66628efc18
finesaaa <indriatifiqey16@gmail.com>
2022-09-18 10:50:25 +0700

    refactor: adjust navbar and sidebar

--- src/resources/views/admin/layouts/main.blade.php
+++ src/resources/views/admin/layouts/main.blade.php
@@ -164,7 +164,7 @@
 <body>
   @include('admin.layouts.header')
   <div class="container-fluid">
-    <div class="row">
+    <div class="row p-3 mx-2">
       @include('admin.layouts.sidebar')
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
