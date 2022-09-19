27eab581b08ea09a57cd63ff6f2e7fa749ffcffc
finesaaa <indriatifiqey16@gmail.com>
2022-09-19 23:39:28 +0700

    refactor: add link href for logo header

--- src/resources/views/admin/layouts/header.blade.php
+++ src/resources/views/admin/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <header class="navbar navbar-white sticky-top bg-white flex-md-nowrap p-7 shadow-sm">
-  <a class="navbar-brand bg-white col-md-3 col-lg-2 me-0 px-4 fs-6" href="#">
+  <a class="navbar-brand bg-white col-md-3 col-lg-2 me-0 px-4 fs-6" href="{{ url('/admin/dashboard') }}">
     <img src="/logokcv.png" alt="logo" height="45"/>
   </a>
   <div class="navbar-nav">
